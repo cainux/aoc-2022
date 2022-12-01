d9d01bb0c7cf41b43a8fa96c0c3f99249199836b
Cain Voong <cainux@gmail.com>
2022-12-01 14:07:58 +0000

    Day 1, I hate this already 🤬

---
+++ src/Day01/Program.cs
@@ -0,0 +1,24 @@
+﻿var sr = File.OpenText("input.txt");
+var elves = new[] {0, 0, 0};
+var runningValue = 0;
+
+while (!sr.EndOfStream)
+{
+    var line = sr.ReadLine();
+
+    if (!string.IsNullOrEmpty(line))
+    {
+        runningValue += Convert.ToInt32(line);
+    }
+    else
+    {
+        if (runningValue > elves[0])
+        {
+            elves[0] = runningValue;
+        }
+        Array.Sort(elves);
+        runningValue = 0;
+    }
+}
+
+Console.WriteLine($"🎄 {elves.Sum()} 🎄");
